620532247f4ed38258bc9aa44c394f9515cc8133
Hadar Sharvit <hadar933@cs.huji.ac.il>
2021-05-10 18:51:47 +0300

    startin, first push

--- Exercises/ex2/cmake-build-debug/CMakeFiles/ex2.dir/DependInfo.cmake
+++ Exercises/ex2/cmake-build-debug/CMakeFiles/ex2.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/cygdrive/c/Users/hadar/Google Drive/year C/semester B/OS/OS/Exercises/ex2/main.cpp" "/cygdrive/c/Users/hadar/Google Drive/year C/semester B/OS/OS/Exercises/ex2/cmake-build-debug/CMakeFiles/ex2.dir/main.cpp.o"
+  "/cs/usr/hadar933/Desktop/OS/OS/Exercises/ex2/uthreads.cpp" "/cs/usr/hadar933/Desktop/OS/OS/Exercises/ex2/cmake-build-debug/CMakeFiles/ex2.dir/uthreads.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- Exercises/ex2/cmake-build-debug/CMakeFiles/ex2.dir/cmake_clean.cmake
+++ Exercises/ex2/cmake-build-debug/CMakeFiles/ex2.dir/cmake_clean.cmake
@@ -1,8 +1,7 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/ex2.dir/main.cpp.o"
-  "ex2.exe"
+  "CMakeFiles/ex2.dir/uthreads.cpp.o"
+  "ex2"
   "ex2.pdb"
-  "libex2.dll.a"
 )
 
 # Per-language clean rules from dependency scanning.
