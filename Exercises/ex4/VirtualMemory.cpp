e9f2d084a0803aa85ccc934b1648e426cc7ef8a4
Hadar Sharvit <hadar933@cs.huji.ac.il>
2021-06-21 14:16:33 +0300

    working on dfs

--- Exercises/ex4/VirtualMemory.cpp
+++ Exercises/ex4/VirtualMemory.cpp
@@ -12,13 +12,47 @@ void VMinitialize() {
 }
 
 
-int dfs() {
-    return 0;
+int dfs(uint64_t depth, uint64_t base, uint64_t path,bool *emptyFrame, uint64_t frameToKeep) {
+    word_t value;
+    int dfsRet; int foundFrame =0;
+    bool valueIsZero = true;
+    if(depth == TABLES_DEPTH) { // recursion base
+        return 0;
+    }
+    for (uint64_t i=0;i<PAGE_SIZE;i++){
+        PMread(i+base*PAGE_SIZE,&value); // read content of frame
+        if(value!=0){ // frame isn't empty
+               uint64_t newPath = (path << OFFSET_WIDTH) +i; // path to the current frame
+               valueIsZero = false; // frame isn't empty boolean indicator
+               if(depth == TABLES_DEPTH -1){ // find path weights here
+
+
+
+               }
+               dfsRet = dfs(depth+1,value,newPath,emptyFrame,frameToKeep); // recursive call for depth+1
+               if(dfsRet!=0 && !foundFrame){ // dfs has found some non empty frame AND didnt found a frame till now
+                   if(*emptyFrame){ // the CHILD frame is currently empty - we can write to it
+                       PMwrite(base*PAGE_SIZE+i,0); // writing to the parent 0 (unlink from parent)
+                       *emptyFrame = false;
+                   }
+                   foundFrame = dfsRet; // updating that we have found a frame
+               }
+        }
+    }
+    if(base!= frameToKeep && base != 0 && valueIsZero){ //making sure to not remove a frame from the path
+        *emptyFrame = true;
+        return base;
+    }
+    return foundFrame;
 }
 
+/**
+ * finds an adress to evict by splitting to various cases
+ * @return
+ */
 uint64_t findAddressToEvict(){
-    int toEvict = dfs();
-    int frameIndex = 0;
+    uint64_t toEvict = dfs();
+    uint64_t frameIndex = 0;
     uint64_t evictedPageIndex = 0;
     uint64_t physicalAddress = 0;
     bool freeFrame = frameIndex + 1 < NUM_FRAMES;
@@ -27,14 +61,12 @@ uint64_t findAddressToEvict(){
             toEvict = frameIndex+1;
             break;
         case false: // no free frame exists, must evict
-            PMevict(toEvict,evictedPageIndex);
+            PMevict(toEvict,evictedPageIndex); //TODO: need to update toEvict
             PMwrite(physicalAddress,0);
             break;
-        default: // found a frame containing an empty table
-            return toEvict;
-
     }
-
+    // found a frame containing an empty table:
+    return toEvict;
 }
 
 
@@ -98,4 +130,63 @@ int VMwrite(uint64_t virtualAddress, word_t value) {
     uint64_t physicalAddress = toPhysicalAddress(virtualAddress);
     PMwrite(physicalAddress,value);
     return 1;
+}
+
+
+
+
+/**
+ * @param forbidden frame should never be evicted
+ * @param baseFrame current frame in the recursion
+ * @param depth current tree depth
+ * @param empty is returned frame empty
+ * @param maxFrame curent max frame index
+ * @param pageIndex of the requierd page
+ * @param route in the recursion tree
+ * @param farFrame current far frame
+ * @param farFrameDist distance of the most far frame
+ * @param farFramePointer pointer to the most far frame
+ * @param farFramePageIndex index of page in the most far frame
+ * @return frame to evict, if found
+ */
+uint64_t DFS(uint64_t forbidden, uint64_t baseFrame, int depth, bool *empty, uint64_t * maxFrame,
+             uint64_t pageIndex, uint64_t route, uint64_t * farFrame, uint64_t * farFrameDist,
+             uint64_t * farFramePointer, uint64_t * farFramePageIndex) {
+    *maxFrame = (*maxFrame > baseFrame) ? *maxFrame : baseFrame;
+    if (depth == TABLES_DEPTH) {
+        return 0;
+    }
+    uint64_t result=0, dfsResult;
+    bool isEmpty = true;
+    word_t value;
+    for (uint64_t i = 0; i < PAGE_SIZE; ++i) {
+        PMread(baseFrame * PAGE_SIZE + i, &value);
+        if (value != 0) {
+            isEmpty = false;
+            uint64_t newRoute = (route << OFFSET_WIDTH) + i;
+            if (depth == TABLES_DEPTH - 1) {
+                uint64_t absDist = (pageIndex > newRoute) ? (pageIndex - newRoute) :
+                                   (newRoute-pageIndex);
+                uint64_t dist = (NUM_PAGES - absDist > absDist) ? absDist : NUM_PAGES - absDist;
+                *farFrameDist = (*farFrameDist > dist) ? *farFrameDist : dist;
+                *farFramePointer = baseFrame * PAGE_SIZE + i;
+                *farFramePageIndex = newRoute;
+                *farFrame = value;
+            }
+            dfsResult = DFS(forbidden, value, depth + 1, empty, maxFrame, pageIndex,
+                            newRoute, farFrame, farFrameDist, farFramePointer, farFramePageIndex);
+            if (!result && dfsResult) {
+                if (*empty) {
+                    *empty = false;
+                    PMwrite(baseFrame * PAGE_SIZE + i, 0);
+                }
+                result = dfsResult;
+            }
+        }
+    }
+    if (isEmpty &&  ((baseFrame != 0) && (baseFrame != forbidden))) {
+        *empty = true;
+        return baseFrame;
+    }
+    return result;
 }
